e8f06282288d36b83af8b350f21b3ce5ba136099
JaeHo Song <thruthesky@gmail.com>
2016-12-05 13:26:34 +0900

    work on firebase

--- firebase-auth.ts
+++ firebase-auth.ts
@@ -10,6 +10,31 @@ export class FirebaseAuth {
 
     /**
      * @note If the new account was created, the user is signed in automatically.
+     * 
+     * 
+     * @code register at philgo first and then register at firebase.
+     * 
+    register() {
+        this.process.begin();
+        this.member.register( this.form, (login) => {
+            console.log('onClickRegister(), registration sucess: ', login );
+            let email = this.member.getApiEmail( login );
+            let password = this.member.getApiPassword( login );
+            this.auth.register( email, password, FirebaseAuth => {
+                // firebase registration ok
+                this.route.go('/');
+            }, (code, message) => {
+                message = 'Warning! Registration Error. Error Code: ' + code + ' : ' + message + ' Please report this error message to admin.';
+                this.process.setError( message );
+            })
+        },
+        e => {
+            console.log("onClickRegister() error: " + e);
+            setTimeout(()=>this.process.setError( e ),345);
+        });
+    }
+    
+     * @endcode
      */
     register( email, password, successCallback, failureCallback ) {
         this.auth.createUserWithEmailAndPassword(email, password)
